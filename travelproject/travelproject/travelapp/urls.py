32b9a743928186de6dda3e106f878c5a28b70751
Brijesh <brijeshbabu183@gmail.com>
2023-10-04 20:12:53 +0530

    addition

--- travelproject/travelproject/travelapp/urls.py
+++ travelproject/travelproject/travelapp/urls.py
@@ -3,7 +3,6 @@ from . import views
 urlpatterns = [
 
     path('',views.demo,name='demo'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact')
+    path('add/',views.addition,name='addition')
 
 ]
--- travelproject/travelproject/travelapp/views.py
+++ travelproject/travelproject/travelapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def demo(request):
-    return render(request,"index.html")
+    name = "india"
+    return render(request,"index.html",{'obj':name})
 
-def about(request):
-    return render(request,"about.html")
+def addition(request):
+    x=int(request.GET['num1'])
+    y=int(request.GET['num2'])
+    res=x+y
+    return render(request,"result.html",{'result':res})
 
-def contact(request):
-    return HttpResponse("Hi guys")
